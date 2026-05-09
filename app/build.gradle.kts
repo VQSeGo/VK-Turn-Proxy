04a513e8f644aa1b63e506702ff951c2e2dfd260
samosvalishe <samosvalishe@users.noreply.github.com>
2026-05-09 12:07:02 +0300

    chore: bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.freeturn.app"
         minSdk = 23
         targetSdk = 36
-        versionCode = 19
-        versionName = "2.2.0"
+        versionCode = 20
+        versionName = "2.2.1"
     }
 
     packaging {
